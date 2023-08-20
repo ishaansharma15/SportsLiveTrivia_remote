4f39e0fd77ffec05acd6fe1a3b21f7e673157189
ab3131 <aravb3@gmail.com>
2023-08-20 16:44:53 -0700

    nbalogo added

--- SportsLiveTrivia/ContentView.swift
+++ SportsLiveTrivia/ContentView.swift
@@ -9,17 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "square")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-                
-            Text("SLT")
-            Text("ab3131 change")
+        
+            Image(systemName: "nba_logo")
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
