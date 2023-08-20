5a4c542f58ac22fcf3d45be6e0d77baf67bbf437
ab3131 <aravb3@gmail.com>
2023-08-20 15:58:06 -0700

    Initial Commit

---
+++ SportsLiveTrivia/SportsLiveTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsLiveTriviaApp.swift
+//  SportsLiveTrivia
+//
+//  Created by Ishaan Sharma on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SportsLiveTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
