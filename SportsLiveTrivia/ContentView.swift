d9df35c241ecd22a10e8fa07da6c0eed031a1353
ishaansharma15 <ishaansh4@gmail.com>
2023-08-20 16:34:12 -0700

    committing first change test

--- SportsLiveTrivia/ContentView.swift
+++ SportsLiveTrivia/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
                 
             Text("SLT")
+            Text("ab3131 change")
         }
         .padding()
     }
