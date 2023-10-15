83ebac2389cb27a42cfb512e4e2e0d60f371696b
ab3131 <aravb3@gmail.com>
2023-10-14 17:58:20 -0700

    fontsize30

--- SportsLiveTrivia/NFLPages/NFLEasyMode.swift
+++ SportsLiveTrivia/NFLPages/NFLEasyMode.swift
@@ -11,7 +11,7 @@ struct NFLEasyMode: View {
     var body: some View {
     
         VStack {
-            Text("NFL Easy Mode")
+            Text("NFL Easy Mode").bold().font(.system(size: 30))
         }
         
     }
