8a5d7faef514e8155b748937d5902740044be472
ishaansharma15 <ishaansh4@gmail.com>
2023-10-21 16:11:49 -0700

    EasyMode for NBA: Button click increments questionNum and changes labels

--- SportsLiveTrivia/NBAPages/EasyMode.swift
+++ SportsLiveTrivia/NBAPages/EasyMode.swift
@@ -12,22 +12,23 @@ struct EasyMode: View {
     var secondColor: Color = Color("SecondaryColor")
     var thirdColor: Color = Color("TertiaryColor")
     var fourthColor: Color = Color("Color4")
+    @State var questionNum: Int = 0
+    let questions = ["Who won the MVP last year?", "Who won the championship last year?"]
+    let answers = ["Jokic","Nuggets"]
+    var max: Int = 2
     var body: some View {
         VStack(spacing: 30){
             Text("NBA").foregroundColor(fourthColor).bold().font(.system(size: 60))
-            Text("Which team won the first NBA Championship?").foregroundColor(.white).bold().font(.system(size: 30))
-            NavigationLink(destination: EasyMode()) {
-                Text("Easy").bold().foregroundColor(.white).frame(maxWidth: 275).padding(.vertical).padding(.horizontal).background(thirdColor).cornerRadius(10).shadow(radius: 10).bold()
+            Text(questions[questionNum]).foregroundColor(.white)
+            Button {
+                if questionNum+1<max{
+                    questionNum+=1
+                }
+                
+            } label: {
+                Label(answers[questionNum], systemImage: ".cloud.rain").labelStyle(.titleOnly).bold().foregroundColor(.white).frame(maxWidth: 275).padding(.vertical).padding(.horizontal).background(background).cornerRadius(10).shadow(radius: 10)
             }
-            NavigationLink(destination: MediumMode()) {
-                Text("Medium").bold().foregroundColor(.white).frame(maxWidth: 275).padding(.vertical).padding(.horizontal).background(thirdColor).cornerRadius(10).shadow(radius: 10).bold()
-            }
-            NavigationLink(destination: HardMode()) {
-                Text("Hard").bold().foregroundColor(.white).frame(maxWidth: 275).padding(.vertical).padding(.horizontal).background(thirdColor).cornerRadius(10).shadow(radius: 10).bold()
-            }
-            NavigationLink(destination: ExtremeMode()) {
-                Text("Extreme").bold().foregroundColor(.white).frame(maxWidth: 275).padding(.vertical).padding(.horizontal).background(thirdColor).cornerRadius(10).shadow(radius: 10).bold()
-            }
+
         }.frame(maxWidth: .infinity, maxHeight: .infinity).edgesIgnoringSafeArea(.all).background(secondColor)
     }
 }
